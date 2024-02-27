5786122ffaee73a0fae619f0d2ba463876a8d3fa
TrojanN63 <fulanodetal@exemplo.br>
2024-02-27 08:27:06 -0300

    código copiado fodase

--- fodase.js
+++ fodase.js
@@ -1,3 +1,15 @@
-function showalert(){
-    alert('fodase')
+function submit(){
+    let confirm = confirm("confirm? ");
+    if (confirm == true){
+        let name = document.getElementById("name").value;
+        let email = document.getElementById("email").value;
+        document.getElementById("result").innerHTML = 
+        '<h3>Informações Recebidas</h3>'+
+        "<p>Nome: " + name + "</p>"+
+        '<p>E-mail:' + email + '</p>'
+    }
+    else {
+        document.getElementById("result").innerHTML =
+        "<h2>Ação cancelada</h2>"
+    }
 }

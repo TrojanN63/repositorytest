4db546b6ea843cc76d6cb41ca94219716e7cba4c
TrojanN63 <fulanodetal@exemplo.br>
2024-02-27 08:27:58 -0300

    mudança

--- fodase.js
+++ fodase.js
@@ -1,15 +1,15 @@
-function submit(){
-    let confirm = confirm("confirm? ");
-    if (confirm == true){
-        let name = document.getElementById("name").value;
+function submeter() {
+    let confirma = confirm("Você confirma a ação?");
+    if (confirma == true) {
+        let nome = document.getElementById("nome").value;
         let email = document.getElementById("email").value;
-        document.getElementById("result").innerHTML = 
-        '<h3>Informações Recebidas</h3>'+
-        "<p>Nome: " + name + "</p>"+
-        '<p>E-mail:' + email + '</p>'
+        document.getElementById("resultado").innerHTML = 
+            "<h3>Informações Recebidas</h3>" +
+            "<p>Nome: " + nome + "</p>" +
+            "<p>E-mail: " + email + "</p>";
     }
     else {
-        document.getElementById("result").innerHTML =
-        "<h2>Ação cancelada</h2>"
+        document.getElementById("resultado").innerHTML = 
+            "<h2>Ação cancelada</h2>";
     }
 }

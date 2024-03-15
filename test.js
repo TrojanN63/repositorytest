451c68a514bb9cb4c7f092fb465ba8ebb406720b
TrojanN63 <alves.tavares@escolar.ifrn.edu.br>
2024-03-15 07:30:06 -0300

    factorial

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
-let sum = 1
-for (let i = 1; i<= 10; i++){
-    sum*=i
+function factorial(){
+    let limit = document.getElementById("number").value;
+    let sum = 1
+    for (i=1;i<=n;i++){
+        sum*=i;
+    }
+    document.getElementById("result").innerHTML = 
+    `Result for ${limit}! = ${sum}`;
 }

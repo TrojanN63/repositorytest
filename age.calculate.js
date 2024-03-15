abb32526f2c764a69bb1d5178433591daab14819
TrojanN63 <alves.tavares@escolar.ifrn.edu.br>
2024-03-15 08:29:56 -0300

    age

---
+++ age.calculate.js
@@ -0,0 +1,9 @@
+var timeInMs = Date.now();
+let now = new Date();
+const year = now.getFullYear()
+const month = now.getMonth() + 1
+const day = now.getDate()
+
+var born = new Date();
+
+console.log(`${day}/${month}/${year}`)

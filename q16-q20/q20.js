4bb02a1d61826e7bd67db38198e7c6b7df54a60f
TrojanN63 <alves.tavares@escolar.ifrn.edu.br>
2024-07-08 08:17:08 -0300

    tentandoterminar

--- q16-q20/q20.js
+++ q16-q20/q20.js
@@ -1,27 +1,42 @@
 function confirm(){
-    email = document.getElementById("email").value;
-    senha = document.getElementById("senha").value;
-    senha2 = document.getElementById("senha2").value;
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+    const senha2 = document.getElementById("senha2").value;
+    var camps = ""
+    var resultemail = ""
+    var resultpassword = ""
 
     if (email == ""){
-        document.getElementById("result").innerHTML = "email não preenchido"
-    }
-    else if (email.indexOf("@") = -1){
-        document.getElementById("result").innerHTML = "email inválido"
-    }
-    else if (email.indexOf(".") = -1){
-        document.getElementById("result").innerHTML = "email inválido"
+        camps += "email não preenchido<br>"
     }
     else if (senha == ""){
-        document.getElementById("result").innerHTML = "senha não preenchida"
+        camps += "senha não preenchida<br>"
     }
-    else if (senha.length < 8){
-        document.getElementById("result").innerHTML = "senha precisa de pelo menos 8 caracteres"
+    else if (senha2 == ""){
+        camps += "senha não confirmada<br>"
+    }
+    else {camps = "todos os campos preenchidos<br>"}
+
+    if (email.indexOf("@") = -1){
+        resultemail = "email inválido<br>"
+    }
+    else if (email.indexOf(".") = -1){
+        resultemail = "email inválido<br>"
+    }
+    else {resultemail = "email válido<br>"}
+
+    if (senha.length < 8){
+        resultpassword = "senha precisa de pelo menos 8 caracteres<br>"
     }
     else if (senha !== senha2){
-        document.getElementById("result").innerHTML = "senha não está igual à confirmação"
+        resultpassword = "senha não está igual à confirmação<br>"
     }
     else {
-        document.getElementById("result").innerHTML = "cadastro concluído, sua alma agora é minha..."
+        document.getElementById("finalresult") = "cadastro concluído, sua alma agora é minha..."
     }
+
+    document.getElementById("camps").innerHTML = camps
+    document.getElementById("resultemail").innerHTML = resultemail
+    document.getElementById("resultpassword").innerHTML = resultpassword
 }
+

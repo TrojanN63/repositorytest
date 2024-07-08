48709c5e5ef497ce13bb3b240b1f4a6293ac50e6
TrojanN63 <alves.tavares@escolar.ifrn.edu.br>
2024-07-08 08:30:27 -0300

    refazendo

--- q16-q20/q20.js
+++ q16-q20/q20.js
@@ -1,42 +1,16 @@
 function confirm(){
+    const nome = document.getElementById("nome").value;
     const email = document.getElementById("email").value;
     const senha = document.getElementById("senha").value;
     const senha2 = document.getElementById("senha2").value;
-    var camps = ""
-    var resultemail = ""
-    var resultpassword = ""
 
-    if (email == ""){
-        camps += "email não preenchido<br>"
-    }
-    else if (senha == ""){
-        camps += "senha não preenchida<br>"
-    }
-    else if (senha2 == ""){
-        camps += "senha não confirmada<br>"
-    }
-    else {camps = "todos os campos preenchidos<br>"}
-
-    if (email.indexOf("@") = -1){
-        resultemail = "email inválido<br>"
-    }
-    else if (email.indexOf(".") = -1){
-        resultemail = "email inválido<br>"
-    }
-    else {resultemail = "email válido<br>"}
-
-    if (senha.length < 8){
-        resultpassword = "senha precisa de pelo menos 8 caracteres<br>"
-    }
-    else if (senha !== senha2){
-        resultpassword = "senha não está igual à confirmação<br>"
-    }
-    else {
-        document.getElementById("finalresult") = "cadastro concluído, sua alma agora é minha..."
-    }
-
-    document.getElementById("camps").innerHTML = camps
-    document.getElementById("resultemail").innerHTML = resultemail
-    document.getElementById("resultpassword").innerHTML = resultpassword
+    if (nome.length == 0){document.getElementById("result").innerHTML = "Nome não preenchido."}
+    else if (email.length == 0){document.getElementById("result").innerHTML = "Email não preenchido."}
+    else if (email.indexOf("@") = -1){document.getElementById("result").innerHTML = "email inválido."}
+    else if (email.indexOf(".") = -1){document.getElementById("result").innerHTML = "email inválido."}
+    else if (senha.length == 0){document.getElementById("result").innerHTML = "Senha não preenchida."}
+    else if (senha.length < 8){document.getElementById("result").innerHTML = "Senha precisa ter pelo menos 8 caractéres."}
+    else if (senha !== senha2){document.getElementById("result").innerHTML = "Senha precisa ser igual à confirmação."}
+    
 }
 

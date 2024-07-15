12b285585d1d798a6503a634ba63c965dd74d0af
TrojanN63 <alves.tavares@escolar.ifrn.edu.br>
2024-07-15 08:28:57 -0300

    wdnjkj

--- calvificador.js
+++
@@ -1,6 +0,0 @@
-function calvificar(){
-    let futuroscalvos = document.getElementById("futuroscalvos").value.split(" ")
-    futuroscalvos.splice(-1,0,"e")
-    let calvos = futuroscalvos.join(" ")
-    alert(calvos)
-}

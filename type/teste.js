5525dbf9d9a3ad0884ca49d87df9a1879acec7db
TrojanN63 <alves.tavares@escolar.ifrn.edu.br>
2024-11-05 11:09:24 -0300

    teste com métodos (odeio typeshit)

--- type/teste.js
+++ type/teste.js
@@ -7,9 +7,22 @@ var Circulo = /** @class */ (function () {
     Circulo.prototype.exibir = function () {
         return "Raio: ".concat(this.raio, ", x: ").concat(this.x, ", y: ").concat(this.y);
     };
+    Circulo.prototype.mover = function (x, y) {
+        this.x += x;
+        this.y += y;
+    };
+    Circulo.prototype.aumentar = function (r) {
+        this.raio += r;
+    };
     return Circulo;
 }());
 var circulo1 = new Circulo(2, 0, 0);
 var circulo2 = new Circulo(3, 5, 2);
+console.log(circulo1.exibir());
+console.log(circulo2.exibir());
+circulo1.mover(2, 2);
+circulo1.aumentar(5);
+circulo2.mover(3, 2);
+circulo2.aumentar(6);
 console.log(circulo1.exibir());
 console.log(circulo2.exibir());
--- type/teste.ts
+++ type/teste.ts
@@ -11,10 +11,28 @@ class Circulo {
     exibir() : string {
         return `Raio: ${this.raio}, x: ${this.x}, y: ${this.y}`;
     }
+
+    mover(x: number, y: number) : void {
+        this.x += x;
+        this.y += y;
+    }
+
+    aumentar(r: number) : void{
+        this.raio += r;
+    }
 }
 
 let circulo1 = new Circulo(2,0,0);
 let circulo2 = new Circulo(3,5,2)
+
+console.log(circulo1.exibir());
+console.log(circulo2.exibir());
+
+circulo1.mover(2,2)
+circulo1.aumentar(5)
+
+circulo2.mover(3,2)
+circulo2.aumentar(6)
 
 console.log(circulo1.exibir());
 console.log(circulo2.exibir());
